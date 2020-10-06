96264cfdcf14f2255d12f8363f63e394f31a1d1d
Neeranjan <neeran92@gmail.com>
2020-10-06 21:57:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = MyClient()
+client.run('NzYzMDMyOTkwMTMyNzMxOTM0.X3xz2Q.sJ0SFWkX63_8uM8gF3lIDGRR3Cs')
