156ef557cebda4fd24472cfc10a4ebbf67c140be
Neeranjan <neeran92@gmail.com>
2020-10-06 22:10:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
+# import discord
+
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print('Logged on as', self.user)
+
+#     async def on_message(self, message):
+#         # don't respond to ourselves
+#         if message.author == self.user:
+#             return
+
+#         if message.content == 'pang':
+#             await message.channel.send('pung')
+
+# client = MyClient()
+# client.run('')
+
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+bot = commands.Bot(command_prefix='>')
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-        if message.content == 'pang':
-            await message.channel.send('pung')
-
-client = MyClient()
-client.run('')
+bot.run('NzYzMDMyOTkwMTMyNzMxOTM0.X3xz2Q.6Km1w2m-SWnQMoQr9Lve1Qr88gY')
